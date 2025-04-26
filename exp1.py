0d5ffe0174ed3dfdfbc7f8accc664d153240850e
rushabh782 <155352920+rushabh782@users.noreply.github.com>
2025-04-26 12:30:37 +0530

    Update exp1.py

--- exp1.py
+++ exp1.py
@@ -1,21 +1,27 @@
 import pandas as pd
-# Load the dataset from a CSV file
+
 df = pd.read_csv('/content/sample_data/winemag-data-130k-v2.csv')
-# Calculate the statistics for each column
-# Select only numerical columns for calculations
-numerical_df = df.select_dtypes(include=['number'])  # Selects columns with numerical data types
+
+# select numerical cols for calculation.calculate statistics of each column
+numerical_df = df.select_dtypes(include=['number'])
 statistics = pd.DataFrame({
-'Mean': numerical_df.mean(),  # Calculates mean for numerical columns
-'Median': numerical_df.median(),  # Calculates median for numerical columns
-'Mode': df.mode().iloc[0],  # Take the first mode in case there are multiple
-'Standard Deviation': numerical_df.std(),  # Calculates std for numerical columns
-'Variance': numerical_df.var(),  # Calculates variance for numerical columns
-'Range': numerical_df.max() - numerical_df.min()  # Calculates range for numerical columns
+    'Mean':numerical_df.mean(),
+    'Mode':numerical_df.mode().iloc[0],
+    'Median':numerical_df.median(),
+    'Standard Deviation':numerical_df.std(),
+    'Variance':numerical_df.var(),
+    'Range':numerical_df.max()-numerical_df.min(),
+    'Q1':numerical_df.quantile(0.25),
+    'Q3':numerical_df.quantile(0.75),
+    '10 Percentile':numerical_df.quantile(0.10),
+    '90 Percentile':numerical_df.quantile(0.90),
 })
-# Set pandas options to display all rows and columns
-pd.set_option('display.max_rows', None)  # Show all rows
-pd.set_option('display.max_columns', None)  # Show all columns
-pd.set_option('display.width', None)  # Automatically adjust the width of the display
-pd.set_option('display.max_colwidth', None)  # Ensure no truncation of column values
-# Print the statistics in a tabular format
+
+#Display all rows and columns
+pd.set_option('display.max_rows',None)
+pd.set_option('display.max_columns',None)
+pd.set_option('display.width',None)
+pd.set_option('display.max_colwidth',None)
+
 print(statistics)
+
